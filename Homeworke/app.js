33918de84c419b7329895619accbafb35158ccf6
VitaliiKisin <121611689+VitaliiKisin@users.noreply.github.com>
2023-02-20 21:44:30 +0400

    Update app.js

--- Homeworke/app.js
+++ Homeworke/app.js
@@ -1,26 +1,44 @@
-function go()
-{
-var name = document.getElementById("name").value
-var age = document.getElementById('age').value
-var review = document.getElementById("review").value
-
-var str = 'Привет: ${name}, вам: $age, ваш отзыв: ${review}';
-
-if (age >= 16 && age < 70) {
- str = str + 'И вам все можно'
-} else {
- str = str + 'Вам надо подрасти'
-};
-
-document.getElementById("out").innerText = str;
-document.getElementById("name").value = "";
-document.getElementById('age').value = "";
-document.getElementById('review').value = ""
-}
-//function send()
+//function go()
 //{
-    //var text =document.getElementById('msg').value;
-    //console.log(text);
-    //document.getElementById('msg').value = "";
+//var name = document.getElementById("name").value
+//var age = document.getElementById('age').value
+//var review = document.getElementById("review").value
+
+//var str = "Привет: ${name}, вам: $age, ваш отзыв: ${review}";
+
+//if (age >= 16 && age < 70) {
+ //str = str + 'И вам все можно'
+//} else {
+ //str = str + 'Вам надо подрасти'
+//};
+
+//document.getElementById("out").innerText = str;
+//document.getElementById("name").value = "";
+//document.getElementById('age').value = "";
+//document.getElementById('review').value = ""
 //}
+function send()
+{
+    var message = document.getElementById('text').value;
+    
+    //console.log(text);
+    (async () => {
+        var response = await fetch('chat.php?message=' + message);
+        var answer = await response.text();
+        
+    }
+)();
+
+
+    document.getElementById('text').value = "";
+   
+}
+function get() {
+    (async () => {
+        var response = await fetch('chat.php');
+        var answer = await response.text();
+        document.getElementById('messages').innerText = answer;
+    }
+)();
+}
  
